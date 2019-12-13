985e8910ef9664240a5127ab0054fe9353aa6ac2
easy89757 <easy89757@126.com>
2019-12-13 17:37:53 +0800

    添加各个服务框架代码。

---
+++ src/main/java/com/cctv/admin/AdminApplication.java
@@ -0,0 +1,19 @@
+package com.cctv.admin;
+
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * @author zb
+ * @create 2019-12-13 11:29
+ */
+@SpringBootApplication
+@EnableEurekaClient
+@EnableAdminServer
+public class AdminApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AdminApplication.class,args);
+    }
+}
